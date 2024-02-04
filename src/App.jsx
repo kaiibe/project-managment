228dad51f06c1e1791a4fb1c458803d1aed48304
kaiibe <kaiibe@mail.ru>
2024-02-04 16:40:06 +0100

    no project components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Sidebar from "./components/Sidebar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
